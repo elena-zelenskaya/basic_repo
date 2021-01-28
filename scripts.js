fb1b68ddf9e0a680db4e3cd87afdccf5bd5cb3c8
Elena Zelenskaya <elena.a.zelenskaya@gmail.com>
2021-01-27 17:36:17 -0800

    added toggling

--- scripts.js
+++ scripts.js
@@ -1,8 +1,13 @@
 function changeColor() {
     document.onmousedown = function(e) {
         document.getElementById("lol").style.backgroundColor="blanchedalmond";
-        document.getElementById("lol").innerHTML="I cannot believe it! I've just changed color!";
+        document.getElementById("lol").innerHTML="I cannot believe it! I've just changed color! Click me one more time!";
+        document.onmousedown = function(e) {
+            document.getElementById("lol").style.backgroundColor="aquamarine";
+            document.getElementById("lol").innerHTML="I like it! Click me again!!!";
+            changeColor();
+        }
 
-}
+    }
 }
 changeColor();
